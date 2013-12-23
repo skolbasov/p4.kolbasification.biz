9c075d35291bfd78b43cb507c8b21361f71ed2c9
Sergey Kolbasov <skolbasov@gmail.com>
2013-12-24 02:04:04 +0400

    Before google

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,14 +1,18 @@
 <h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
 
-Sblog=Simple Blog<br><br>
+<blockquote>
+  <p>What is important is seldom urgent and what is urgent is seldom important.</p>
+  <small>U.S. President Dwight D. Eisenhower</small>
+</blockquote>
 
-Additional features that are implemented:
-<br><br>
 
-Email confirmation upon sign-up
-<br>
-Email verification (requires users to click a link in their email before their account is created).
-<br>
-Like feature
-<br>
-@media queries for displays thiner than 980px and 480px
+<p>Time management is the act or process of planning and exercising conscious control over the amount of time spent on specific activities, especially to increase effectiveness, efficiency or productivity. Time management may be aided by a range of skills, tools, and techniques used to manage time when accomplishing specific tasks, projects and goals complying with a due date. Initially, time management referred to just business or work activities, but eventually the term broadened to include personal activities as well. A time management system is a designed combination of processes, tools, techniques, and methods. Time management is usually a necessity in any project development as it determines the project completion time and scope.<p>
+<p>Time management is often considered necessary because:</p>
+<ol>
+  	<li>Available time is limited.</li>
+  	<li>Time cannot be stored: if unused it is lost forever.</li>
+	<li>One's goals are usually multiple, sometimes conflict, and not all goals are of equal priority.</li>
+	<li>Goals cannot be accomplished without the application of effort, which requires the use of time.</li>
+</ol>
+
+<p>All tasks are evaluated using the criteria important/unimportant and urgent/not urgent and put in according quadrants. Tasks in unimportant/not urgent quadrants are dropped, tasks in important/urgent quadrants are done immediately and personally, tasks in unimportant/urgent quadrants are delegated and tasks in important/not urgent quadrants get an end date and are done personally.</p>
