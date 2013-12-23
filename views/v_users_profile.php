cea2e4367f7fe86f645f2b70506bdd137bbc7786
Sergey Kolbasov <skolbasov@gmail.com>
2013-12-24 03:29:55 +0400

    danger button

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -4,6 +4,6 @@
 First name: <?=htmlentities($user->first_name, ENT_QUOTES, 'UTF-8')?><br>
 Last name: <?=htmlentities($user->last_name, ENT_QUOTES, 'UTF-8')?><br>
 
+<button id="authorize-button" class="btn btn-danger">Authorize with google id first</button>
 <button id="googleSync" class="btn btn-primary">Pull events from google calendar</button>
-<button id="authorize-button" class="btn-danger">Authorize with google id first</button>
 <div id="syncStat" class="has-success"></div>
