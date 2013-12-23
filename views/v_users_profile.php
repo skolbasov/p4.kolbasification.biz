81e94fcf675799374277067bfaa887bfc906f5fc
Sergey Kolbasov <skolbasov@gmail.com>
2013-12-24 03:03:16 +0400

    google pull stuff

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -4,5 +4,6 @@
 First name: <?=htmlentities($user->first_name, ENT_QUOTES, 'UTF-8')?><br>
 Last name: <?=htmlentities($user->last_name, ENT_QUOTES, 'UTF-8')?><br>
 
-<button id="googleSync" class="btn btn-primary">Sync events with google calendar</button>
+<button id="googleSync" class="btn btn-primary">Pull events from google calendar</button>
 <button id="authorize-button" class="btn-danger">Authorize with google id first</button>
+<div id="syncStat" class="has-success"></div>
